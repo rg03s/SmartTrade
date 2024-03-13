49f4f3d3ce0b112cd25311df275b0e8289c40b74
Rubén García Sánchez <rubengs.informatica@gmail.com>
2024-03-13 10:11:40 +0100

    Agregar archivos de proyecto.

---
+++ SmartTrade/SmartTrade/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using SmartTrade.ViewModels;
+using SmartTrade.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SmartTrade
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ SmartTrade/SmartTrade/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SmartTrade.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SmartTrade.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
