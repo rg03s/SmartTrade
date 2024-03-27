71e19d4ffa2ed92116582e50bfad35fa57f7f907
Rubén García <rubengs.informatica@gmail.com>
2024-03-27 18:31:01 +0100

    pruebas supabase

--- SmartTrade/SmartTrade/Models/Producto.cs
+++ SmartTrade/SmartTrade/Models/Producto.cs
@@ -1,26 +1,30 @@
-﻿using System;
+﻿using Postgrest.Attributes;
+using Postgrest.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
 
 namespace SmartTrade.Models
 {
-    public partial class Producto
+    public partial class Producto : BaseModel
     {
+        [PrimaryKey("id")]
         public int Id { get; set; }
 
+        [Column("nombre")]
         public string Nombre { get; set; }
-
+        [Column("huella_ecologica")]
         public string Huella_eco { get; set; }
-
+        [Column("imagen")]
         public Image Imagen { get; set; }
-
+        [Column("modelo3d")]
         public Image Modelo3d { get; set; }
-
+        [Column("descripcion")]
         public string Descripcion { get; set; }
-
+        [Column("puntos")]
         public int Puntos { get; set; }
-    
+        [Column("producto_vendedores")]
         public ICollection<Producto_vendedor> Producto_vendedores { get;}
     }
 }
