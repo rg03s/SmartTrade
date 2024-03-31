29a9c0824c9dd0963805566e78e1153cb610b920
Natalia García Duro <NGARDUR@upv.edu.es>
2024-04-01 01:36:26 +0200

    aa

--- SmartTrade/SmartTrade/App.xaml.cs
+++ SmartTrade/SmartTrade/App.xaml.cs
@@ -11,9 +11,7 @@ namespace SmartTrade
 
         public App()
         {
-            string url = "https://apjeqdhvkthosokvpvma.supabase.co";
-            string key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwamVxZGh2a3Rob3Nva3Zwdm1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkyODg4NjIsImV4cCI6MjAyNDg2NDg2Mn0.f7CoRtVVO60qER7HzCtcZyELhSEf_GbdMsSYiJq_7iE";
-
+           
             InitializeComponent();
             STService service = new STService(new STDAL(new ConexionSupabase()));
             DependencyService.Register<MockDataStore>();
