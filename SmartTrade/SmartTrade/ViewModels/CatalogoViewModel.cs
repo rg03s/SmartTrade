b3865163852d987ac6fd7a10ca0c0fa5b1425a71
NotPowah <elrandomgamer@gmail.com>
2024-04-02 12:33:19 +0200

    primera version .xaml

---
+++ SmartTrade/SmartTrade/ViewModels/CatalogoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using SmartTrade.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SmartTrade.ViewModels
+{
+    public class CatalogoViewModel : BaseViewModel
+    {
+
+        public CatalogoViewModel()
+        {
+            Title = "Catalogo";
+        }
+    }
+}
