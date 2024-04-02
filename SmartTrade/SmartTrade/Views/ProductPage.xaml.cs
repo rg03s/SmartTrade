5c8eac29dc33ba7e02dc9567c7ab69b5b1264744
Rubén García <rubengs.informatica@gmail.com>
2024-04-02 18:16:56 +0200

    Diseño de la pantalla de información del producto

---
+++ SmartTrade/SmartTrade/Views/ProductPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using SmartTrade.Persistencia.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartTrade.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductPage : ContentPage
+    {
+
+        private STService service;
+
+        public ProductPage(STService service)
+        {
+            InitializeComponent();
+            this.service = service;
+        }
+
+        private void BtnAtras_click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Atras");
+        }
+
+        private void BtnCarrito_click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Carrito");
+        }
+
+        private void BtnPerfil_click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Perfil");
+        }
+
+        private void BtnAgregarCarrito_click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Carrito");
+        }
+        private void BtnModelo3d_click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Modelo 3D");
+        }
+
+        private void BtnVerComentarios_click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Ver Comentarios");
+        }
+    }
+}
