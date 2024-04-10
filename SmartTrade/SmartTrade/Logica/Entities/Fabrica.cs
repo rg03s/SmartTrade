d74497e7e3d2290434221f63e0ce9045ff9007b9
Rubén García <rubengs.informatica@gmail.com>
2024-04-10 17:48:48 +0200

    método Fábrica mejorado

--- SmartTrade/SmartTrade/Logica/Entities/Fabrica.cs
+++ SmartTrade/SmartTrade/Logica/Entities/Fabrica.cs
@@ -9,23 +9,27 @@ namespace SmartTrade.Logica.Entities
 {
     public class Fabrica : IFabrica
     {
-        public Producto CrearProducto(string categoria, string nombre, string huella, string imagen, string modelo3d, string desc, int puntos, Vendedor vend, Categoria cat, int stock, int precio, Dictionary<string, object> atributosEspecificos)
+        public Producto CrearProducto(string nombre, string huella, string imagen, string modelo3d, string desc, int puntos, Vendedor vend, Categoria cat, int stock, int precio, Dictionary<string, object> atributosEspecificos)
         {
-            switch (categoria.ToLower())
+            switch (cat.Nombre.ToLower())
             {
                 case "deporte":
                     string tipo = atributosEspecificos["tipo"] as string;
                     return new Deporte(nombre, huella, imagen, modelo3d, desc, puntos, vend, cat, stock, precio, tipo);
                 case "ropa":
-                    int talla = Convert.ToInt32(atributosEspecificos["talla"]);
+                    string talla = atributosEspecificos["talla"] as string;
                     string color = atributosEspecificos["color"] as string;
-                    return new Ropa(nombre, huella, imagen, modelo3d, desc, puntos, vend, cat, stock, precio, talla, color);
+                    string marca = atributosEspecificos["marca"] as string;
+                    string tipoPrenda = atributosEspecificos["tipoPrenda"] as string;
+                    return new Ropa(nombre, huella, imagen, modelo3d, desc, puntos, vend, cat, stock, precio, talla, color, marca, tipoPrenda);
                 case "papeleria":
                     string material = atributosEspecificos["material"] as string;
                     return new Papeleria(nombre, huella, imagen, modelo3d, desc, puntos, vend, cat, stock, precio, material);
                 case "tecnologia":
-                    string dispositivo = atributosEspecificos["dispositivo"] as string; 
-                    return new Tecnologia(nombre, huella, imagen, modelo3d, desc, puntos, vend, cat, stock, precio, dispositivo);
+                    string dispositivo = atributosEspecificos["dispositivo"] as string;
+                    string marca_dispo = atributosEspecificos["marca"] as string;
+                    string modelo = atributosEspecificos["modelo"] as string;
+                    return new Tecnologia(nombre, huella, imagen, modelo3d, desc, puntos, vend, cat, stock, precio, dispositivo, marca_dispo, modelo);
                 default:
                     throw new ArgumentException("Categoría del producto desconocida o no disponible");
             }
