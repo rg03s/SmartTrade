46c1966a1ed7fac79632d7f3b9dbac8d764353c9
NotPowah <elrandomgamer@gmail.com>
2024-05-31 00:58:45 +0200

    nickname no puede cambiar

--- SmartTrade/SmartTrade/Logica/Entities/Tarjeta.cs
+++ SmartTrade/SmartTrade/Logica/Entities/Tarjeta.cs
@@ -9,7 +9,7 @@ namespace SmartTrade.Entities
     {
         public Tarjeta() { }
 
-        public Tarjeta(int numero, DateTime fecha_cad, int num_seguridad, string nick_comprador) 
+        public Tarjeta(int numero, DateTime fecha_cad, int num_seguridad, string nick_comprador)
         {
             this.Numero = numero;
             this.Fecha_cad = fecha_cad;
--- SmartTrade/SmartTrade/Views/Perfil.xaml.cs
+++ SmartTrade/SmartTrade/Views/Perfil.xaml.cs
@@ -84,49 +84,13 @@ namespace SmartTrade.Views
 
         private async void BtnModificar_click(object sender, EventArgs e)
         {
-            if (UsuarioEntry.Text == service.GetLoggedNickname() && ContraseñaEntry.Text == service.GetLoggedPassword() && EmailEntry.Text == service.GetLoggedEmail())
-                await DisplayAlert("Error", "No se ha realizado ningún cambio!", "ACEPTAR");
+            if (ContraseñaEntry.Text == service.GetLoggedPassword() && EmailEntry.Text == service.GetLoggedEmail())
+                await DisplayAlert("Error", "¡No se ha realizado ningún cambio!", "ACEPTAR");
             else 
             {
                 if (!await DisplayAlert("Confirmación", "¿Está seguro de que desea realizar cambios en su cuenta?", "SI", "NO")) return;
                 Usuario usuario = service.GetLoggedUser();
                 
-                if (UsuarioEntry.Text != service.GetLoggedNickname())
-                {
-                    // Hace falta cambiar todas las claves ajenas al nickname del usuario antes de poder cambiarlo
-                    // en la base de datos. Creo que ya sé para qué sirven las ids numéricas.
-                    List<ItemCarrito> carrito = await service.GetCarrito();
-                    List<ListaDeseosItem> listaDeseos = await service.GetListaDeseos(service.GetLoggedNickname());
-                    List<Pedido> pedidos = await service.GetPedidos();
-                    List<Producto_vendedor> productosVendedor = await service.GetLoggedProductosVendedor();
-                    List<Tarjeta> tarjetas = await service.getTarjetas();
-                    foreach (ItemCarrito ic in carrito)
-                    {
-                        ic.NicknameUsuario = UsuarioEntry.Text;
-                    }
-
-                    foreach (ListaDeseosItem ldi in listaDeseos)
-                    {
-                        ldi.NickPropietario = UsuarioEntry.Text;
-                    }
-
-                    foreach (Pedido p in pedidos)
-                    {
-                        p.NickComprador = UsuarioEntry.Text;
-                    }
-
-                    foreach (Producto_vendedor pv in productosVendedor)
-                    {
-                        pv.NicknameVendedor = UsuarioEntry.Text;
-                    }
-
-                    foreach (Tarjeta t in tarjetas)
-                    {
-                        t.Nick_comprador = UsuarioEntry.Text;
-                    }
-                    
-                    usuario.Nickname = UsuarioEntry.Text;
-                }
                 if (ContraseñaEntry.Text != service.GetLoggedPassword())
                 {
                     usuario.Password = ContraseñaEntry.Text;
